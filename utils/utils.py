3efb98ea971a1188735da35c38c5a1bbaf978ca7
Vincent <vuhh2002@outlook.com>
2023-05-27 15:30:51 +0700

    save profit table to files

--- utils/utils.py
+++ utils/utils.py
@@ -308,14 +308,16 @@ def normalize_single_file(log_path):
     print(csv_file)
 
 
-def profit_table(solution_folder):
+def profit_table(solution_folder, save_folder):
     from pathlib import Path
     import pandas as pd
     import itertools
     import os
     from tqdm import tqdm
+    import pickle
 
     solution_folder = Path(solution_folder)
+    save_folder = Path(save_folder)
 
     tsp_base = [
         "eil51",
@@ -377,7 +379,6 @@ def profit_table(solution_folder):
                 / f"{col_name}_{repeat_time}.thop.sol.log"
             )
             if not os.path.isfile(file_path):
-                print("haha")
                 continue
 
             with open(file_path, "r") as f:
@@ -391,5 +392,9 @@ def profit_table(solution_folder):
 
     pbar.close()
     res_table = pd.DataFrame(res_table)
-    print(res_table.describe().T.to_markdown())
+    with open(save_folder / "profit_table.pkl", "wb") as f:
+        pickle.dump(res_table, f)
+    with open(save_folder / "profit_table.md", "w") as f:
+        f.write(res_table.describe().T.to_markdown())
+    
     return res_table
